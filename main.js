286f4e897aa9400f3f9aa9e7080825d0c1d60427
Jabjubinski <jabajnldz@gmail.com>
2024-12-11 16:38:39 +0400

    Added div creator function to javascript

---
+++ main.js
@@ -0,0 +1,11 @@
+function divs(){
+    
+    for(i = 0; i <= 16; i++){
+    
+        let div = document.createElement('div')
+
+        div.classList.add("card")
+
+        div.innerHTML = `Hello World`
+    }  
+}
