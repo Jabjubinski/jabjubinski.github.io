7fdb5ec9740ce851738881708ee09038f55c7209
Jabjubinski <jabajnldz@gmail.com>
2024-12-11 17:50:32 +0400

    fixed 16x16 grid added hover effect etc.

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 const divContainer = document.querySelector(".container");
+const clear = document.getElementById('clear');
 
 function divs() {
-  for (i = 0; i <= 256; i++) {
-    let card = document.createElement("div");
+  for (i = 0; i < 256; i++) {
+    const card = document.createElement("div");
 
     card.classList.add("myCard");
+
+    card.innerHTML = ``;
 
     card.setAttribute("data-index", i);
 
     divContainer.appendChild(card);
+
+    card.addEventListener('mouseenter', () => {
+      card.style = "background-color: red";
+    });
+    clear.addEventListener('click', () =>{
+        card.style = '';
+    });
   }
+  
 }
 
 divs();
+
+
