dbb07fcf0552aea36f2af5d4bd6c7173c1733ee3
Jabjubinski <jabajnldz@gmail.com>
2024-12-19 16:41:34 +0400

    added custom cursors for modes, fixed bugs etc.

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const divContainer = document.querySelector(".container");
 const gridSizeValue = document.querySelector(".grid-size-value");
 //buttons
 const clear = document.getElementById("clear");
-const btnCreate = document.getElementById("btn-create");
 const rainbow = document.getElementById("rainbow");
 const monoChrome = document.getElementById("monochrome");
 const color = document.getElementById("color");
@@ -27,43 +26,45 @@ function gridMaker() {
   }
   const allPixel = document.querySelectorAll(".pixel");
 
-rainbow.addEventListener("click", () => {
-  allPixel.forEach((pixel) => {
-    pixel.addEventListener("click", () => {
-      const r = Math.floor(Math.random() * 257);
-      const g = Math.floor(Math.random() * 257);
-      const b = Math.floor(Math.random() * 257);
+  rainbow.addEventListener("click", () => {
+    divContainer.style.cursor = "url('pencil-solid.svg')7 500, auto";
+    allPixel.forEach((pixel) => {
+      pixel.addEventListener("click", () => {
+        const r = Math.floor(Math.random() * 257);
+        const g = Math.floor(Math.random() * 257);
+        const b = Math.floor(Math.random() * 257);
 
-      pixel.style.backgroundColor = `rgb(${r},${g},${b})`;
+        pixel.style.backgroundColor = `rgb(${r},${g},${b})`;
+      });
     });
   });
-});
-monoChrome.addEventListener("click", () => {
-  allPixel.forEach((pixel) => {
-    pixel.addEventListener("click", () => {
-      pixel.style.backgroundColor = `${color.value}`;
+  monoChrome.addEventListener("click", () => {
+    divContainer.style.cursor = "url('pencil-solid.svg')7 500, auto";
+    allPixel.forEach((pixel) => {
+      pixel.addEventListener("click", () => {
+        pixel.style.backgroundColor = `${color.value}`;
+      });
     });
   });
-});
-eraser.addEventListener("click", () => {
-  pixel.forEach((pixel) => {
-    pixel.addEventListener("click", () => {
+  eraser.addEventListener("click", () => {
+    divContainer.style.cursor = "url('eraser-solid.svg')7 500, auto";
+    allPixel.forEach((pixel) => {
+      pixel.addEventListener("click", () => {
+        pixel.style.backgroundColor = `${backgroundColor.value}`;
+      });
+    });
+  });
+  backgroundColor.addEventListener("input", () => {
+    allPixel.forEach((pixel) => {
       pixel.style.backgroundColor = `${backgroundColor.value}`;
     });
   });
-});
-backgroundColor.addEventListener("input", () => {
-  allPixel.forEach((pixel) => {
-    pixel.style.backgroundColor = `${backgroundColor.value}`;
-  });
-});
 
-clear.addEventListener("click", () => {
-  allPixel.forEach((pixel) => {
-    pixel.style.backgroundColor = `${backgroundColor.value}`;
+  clear.addEventListener("click", () => {
+    allPixel.forEach((pixel) => {
+      pixel.style.backgroundColor = `${backgroundColor.value}`;
+    });
   });
-});
-
 }
 
 gridNumber.addEventListener("input", () => {
@@ -73,4 +74,3 @@ gridNumber.addEventListener("input", () => {
 window.onload = () => {
   gridMaker();
 };
-
