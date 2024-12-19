be8a7d0e2cf94f5285d14e0bd1555470557674bf
Jabjubinski <jabajnldz@gmail.com>
2024-12-19 15:07:49 +0400

    fixed flexbox grid issues, made input responsive

--- main.js
+++ main.js
@@ -11,60 +11,70 @@ const eraser = document.getElementById("eraser");
 const backgroundColor = document.getElementById("background-color");
 //grid rows and columns inputs
 const gridNumber = document.getElementById("grid-number");
+
+//grid pixel number changer event listener
 gridNumber.addEventListener("input", () => {
   gridSizeValue.innerHTML = `${gridNumber.value} x ${gridNumber.value}`;
-});
-//grid making function
-btnCreate.addEventListener("click", () => {
   divContainer.innerHTML = "";
 
-  if (gridNumber.value < 1) {
-    console.error("The input is invalid");
-    alert("grid has to be bigger or equal to 1");
-    return null;
-  }
-  if (gridNumber.value > 100) {
-    console.error("The input is invalid");
-    alert("grid has to be less or equal to 100");
-    return null;
-  }
-
-  for (i = 0; i < gridNumber.value * gridNumber.value; i++) {
+  for (let i = 0; i < gridNumber.value * gridNumber.value; i++) {
     const pixel = document.createElement("div");
 
     pixel.classList.add("pixel");
-    pixel.style.width = `calc(100/${gridNumber.value})`;
+    pixel.style.width = `calc(100%/${gridNumber.value})`;
+    pixel.style.backgroundColor = `${backgroundColor.value}`;
     pixel.setAttribute("data-index", i);
     divContainer.appendChild(pixel);
-
-    rainbow.addEventListener("click", () => {
-      pixel.addEventListener("click", () => {
-        const r = Math.floor(Math.random() * 257);
-        const g = Math.floor(Math.random() * 257);
-        const b = Math.floor(Math.random() * 257);
-
-        pixel.style.backgroundColor = `rgb(${r},${g},${b})`;
-      });
-    });
-    monoChrome.addEventListener("click", () => {
-      pixel.addEventListener("click", () => {
-        pixel.style.backgroundColor = `${color.value}`;
-      });
-    });
-    eraser.addEventListener("click", () => {
-      pixel.addEventListener("click", () => {
-        pixel.style.backgroundColor = "rgb(180, 180, 180)";
-      });
-    });
-    backgroundColor.addEventListener('input', ()=>{
-      pixel.style.backgroundColor = `${backgroundColor.value}`
-    });
   }
+});
+//grid making function
+const allPixel = document.querySelectorAll(".pixel");
+
+rainbow.addEventListener("click", () => {
+  allPixel.forEach((pixel) => {
+    pixel.addEventListener("click", () => {
+      const r = Math.floor(Math.random() * 257);
+      const g = Math.floor(Math.random() * 257);
+      const b = Math.floor(Math.random() * 257);
+
+      pixel.style.backgroundColor = `rgb(${r},${g},${b})`;
+    });
+  });
+});
+monoChrome.addEventListener("click", () => {
+  allPixel.forEach((pixel) => {
+    pixel.addEventListener("click", () => {
+      pixel.style.backgroundColor = `${color.value}`;
+    });
+  });
+});
+eraser.addEventListener("click", () => {
+  pixel.forEach((pixel) => {
+    pixel.addEventListener("click", () => {
+      pixel.style.backgroundColor = `${backgroundColor.value}`;
+    });
+  });
+});
+backgroundColor.addEventListener("input", () => {
+  allPixel.forEach((pixel) => {
+    pixel.style.backgroundColor = `${backgroundColor.value}`;
+  });
 });
 
 clear.addEventListener("click", () => {
-  const allPixel = document.querySelectorAll(".pixel");
   allPixel.forEach((pixel) => {
-    pixel.style.backgroundColor = "rgb(180, 180, 180)";
+    pixel.style.backgroundColor = `${backgroundColor.value}`;
   });
 });
+
+window.onload = () => {
+  for (let i = 0; i < 16 * 16; i++) {
+    const pixel = document.createElement("div");
+
+    pixel.classList.add("pixel");
+    pixel.style.width = `calc(100%/${gridNumber.value})`;
+    pixel.style.backgroundColor = `${backgroundColor.value}`;
+    pixel.setAttribute("data-index", i);
+    divContainer.appendChild(pixel);
+  }
+};
